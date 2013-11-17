ac493d619411a7916842af8a73bfb2e83f7096a9
Todd Wolfson <todd@twolfson.com>
2013-11-16 22:16:20 -0800

    Completed test outlines

--- test/gifsockets-server_test.js
+++ test/gifsockets-server_test.js
@@ -1,3 +1,25 @@
-describe('A gifsockets-server', function () {
+describe('A request to a gifsockets-server', function () {
   // TODO: We need to build a way to close open connections
+
+  describe('writing a text frame', function () {
+    it('receives a new frame', function () {
+
+    });
+  });
+
+  describe('writing a raw frame', function () {
+    it('receives a new frame', function () {
+
+    });
+  });
+});
+
+describe('A request to a gifsockets-server', function () {
+  describe('writing a first frame', function () {
+    describe('and a second frame', function () {
+      it('receives both frames', function () {
+
+      });
+    });
+  });
 });
