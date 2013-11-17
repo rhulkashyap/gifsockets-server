cfe7660d0e978a91335ccd1a7c08fb0b08f11519
Todd Wolfson <todd@twolfson.com>
2013-11-16 22:20:50 -0800

    Creating set up for tests

--- test/gifsockets-server_test.js
+++ test/gifsockets-server_test.js
@@ -1,5 +1,31 @@
+var GifsocketsServer = require('../');
+var pixelServerPath = require.resolve('phantomjs-pixel-server');
+
+before(function startPhantomPixelServer (done) {
+  this._phantomServer = spawn('phantomjs', [pixelServerPath], {stdio: [0, 1, 2]});
+  setTimeout(done, 1000);
+});
+before(function (done) {
+  this.server = new GifsocketsServer();
+  this.server.listen(7050);
+  setTimeout(function () {
+    done();
+  }, 100);
+});
+after(function (done) {
+  this.server.destroy(function (err) {
+    done();
+  });
+});
+after(function (done) {
+  this._phantomServer.kill();
+  this._phantomServer.on('exit', function (code, signal) {
+    done();
+  });
+});
+
 describe('A request to a gifsockets-server', function () {
-  // TODO: We need to build a way to close open connections
+  // TODO: We need to build a way to close open connections (e.g. POST endpoint)
 
   describe('writing a text frame', function () {
     it('receives a new frame', function () {
