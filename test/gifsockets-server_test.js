5ad0ce32e2ec20d2b39b44ecddc08f2212b2827e
Todd Wolfson <todd@twolfson.com>
2013-11-16 22:37:37 -0800

    Filling out tests

--- test/gifsockets-server_test.js
+++ test/gifsockets-server_test.js
@@ -1,5 +1,6 @@
 var spawn = require('child_process').spawn;
 var pixelServerPath = require.resolve('phantomjs-pixel-server');
+var request = require('request');
 var GifsocketsServer = require('../');
 
 before(function startPhantomPixelServer (done) {
@@ -27,10 +28,47 @@ after(function (done) {
 
 describe('A request to a gifsockets-server', function () {
   // TODO: We need to build a way to close open connections (e.g. POST endpoint)
+  before(function (done) {
+    var that = this;
+    var req = request({
+      url: 'http://localhost:7050/image.gif'
+    });
+    req.on('error', function (err) {
+      done(err);
+    });
+    req.on('response', function (res) {
+      that.gifRes = res;
+      done();
+    });
+  });
 
   describe('writing a text frame', function () {
-    it('receives a new frame', function () {
+    before(function saveGifData () {
+      var that = this;
+      this.gifData = '';
+      this.gifRes.on('data', function (buff) {
+        that.gifData += buff;
+      });
+    });
+    before(function writeNewFrame (done) {
+      this.timeout(5000);
+      request({
+        url: 'http://localhost:7050/image/text',
+        method: 'POST',
+        form: {
+          text: 'Hello'
+        }
+      }, function (err, res, body) {
+        if (err) {
+          return done(err);
+        }
+        // TODO: We should callback when we have written out the frame
+        setTimeout(done, 1000);
+      });
+    });
 
+    it('receives a new frame', function () {
+      console.log(this.gifData);
     });
   });
 
